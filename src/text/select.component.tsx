9522ef710b42086e026fa3f4066b46fdc162dc61
ascoders <576625322@qq.com>
2017-05-27 17:48:58 +0800

    add select

---
+++ src/text/select.component.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { Props, State } from "./select.type"
+
+export class Text extends React.Component<Props, State> {
+  public static defaultProps = new Props()
+  public state = new State()
+
+  public render() {
+    const Options = this.props.options.map((option, index) => {
+      return <option value={option.value}>{option.name}</option>
+    })
+
+    return (
+      <select defaultValue={this.props.defaultValue}>
+        {Options}
+      </select>
+    )
+  }
+}
---
+++ src/text/select.type.ts
@@ -0,0 +1,38 @@
+export class Props {
+  public gaeaSetting = {
+    key: "gaea-text",
+    name: "下拉选择",
+    editor: [
+      "基本",
+      {
+        field: "defaultValue",
+        label: "默认值",
+        type: "text"
+      },
+      {
+        field: "options",
+        label: "选项",
+        type: "array",
+        arrayType: {
+          value: "string",
+          name: "string"
+        }
+      }
+    ]
+  }
+
+  public style: React.CSSProperties = {
+    color: "#333",
+    fontSize: 14
+  }
+
+  public defaultValue: string = null
+  public options: IOption[] = []
+}
+
+export class State { }
+
+export interface IOption {
+  value: string
+  name: string
+}
