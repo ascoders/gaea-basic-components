a9ac290071bf399b47aaa9f4832fe957b67f1a6c
ascoders <576625322@qq.com>
2017-05-27 17:58:15 +0800

    add select

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Container } from "./container/container.component"
 import { Text } from "./text/text.component"
+import { Select } from "./select/select.component"
 
-export default [Container, Text] as any[]
+export default [Container, Text, Select] as any[]
--- src/select/select.component.tsx
+++ src/select/select.component.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Props, State } from "./select.type"
 
-export class Text extends React.Component<Props, State> {
+export class Select extends React.Component<Props, State> {
   public static defaultProps = new Props()
   public state = new State()
 
