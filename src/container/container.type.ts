06046e28e269256fe9ca949595411b8a850d04fd
ascoders <576625322@qq.com>
2017-06-04 18:45:55 +0800

    layout 新增背景色、透明度 配置

--- src/container/container.type.ts
+++ src/container/container.type.ts
@@ -3,6 +3,25 @@ export class Props {
     key: "gaea-container",
     name: "容器",
     isContainer: true,
+    editors: [
+      "基本",
+      {
+        field: "style.backgroundColor",
+        label: "背景颜色",
+        type: "color"
+      },
+      {
+        field: "style.opacity",
+        label: "透明度",
+        type: "number",
+        data: {
+          useSlider: true,
+          sliderStep: 1,
+          inputRange: [0, 100],
+          outputRange: [0, 1]
+        }
+      }
+    ]
   }
 
   public style: React.CSSProperties = {
