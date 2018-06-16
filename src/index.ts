fd11d716af65f0fe19dba7e6ad86ccd1790e78c4
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-06-16 21:10:06 +0800

    update

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Container } from "./container/container.component"
-import { Text } from "./text/text.component"
-import { Select } from "./select/select.component"
-
-export default [Container, Text, Select] as any[]
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from './button';
+import { Container } from './container';
+import { Icon } from './icon';
+import { Select } from './select';
+import { Switch } from './switch';
+
+export default [Container, Button, Select, Icon, Switch] as any[];
